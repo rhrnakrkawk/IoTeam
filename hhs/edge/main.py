dc9ff9e35a27cd63729a275598127a77b09eaf08
heesane <tees3359@gmail.com>
2023-06-08 00:17:43 +0900

    Edge Server

--- hhs/edge/main.py
+++ hhs/edge/main.py
@@ -7,8 +7,8 @@ async def root():
     return "Edge Server"
 
 @app.get("/order")
-async def order(table_id:int, menu:str, quantity:int):
-    content = f"{table_id} {menu} {quantity}\n"
+async def order(table_id:int, menu:str, amount:int):
+    content = f"{table_id} {menu} {amount}\n"
     
     with open("order.txt", "a") as file:
         file.write(content)
@@ -30,38 +30,48 @@ async def get_order():
     with open("order.txt", "r") as file:
         content = file.read()
     
-    # json = []
-    # if content is None:
-    #     return "No "
-    # for line in content.split("\n"):
-    #     if line == "":
-    #         continue
-    #     line = line.split(" ")
+    json = []
+    if content is None:
+        return "No Data"
+    for line in content.split("\n"):
+        if line == "":
+            continue
+        line = line.split(" ")
         
-    #     table_id = line[0]
-    #     menu = line[1]
-    #     quantity = line[2]
+        table_id = line[0]
+        menu = line[1]
+        quantity = line[2]
         
-    #     json.append({"table_id":table_id, "menu":menu, "quantity":quantity})
-    return content
+        json.append({"table_id":table_id, "menu":menu, "amount":quantity})
+    return json
 
 @app.get("/get/call")
 async def get_call():
     with open("call.txt", "r") as file:
         content = file.read()
     
-    
+    json = []
+    if content is None:
+        return "No Data"
+    for line in content.split("\n"):
+        if line == "":
+            continue
+        line = line.split(" ")
+        
+        table_id = line[0]
+        call = line[1]
+        
+        
+        json.append({"table_id":table_id, "call":call})
         
     return content
 
-@app.get("/clear/order")
+@app.get("/clear")
 async def clear():
     with open("order.txt", "w") as file:
         file.write("")
-    return "success"
-
-@app.get("/clear/call")
-async def clear():
+        
     with open("call.txt", "w") as file:
         file.write("")
+        
     return "success"
